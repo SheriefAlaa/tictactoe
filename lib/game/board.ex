1e7a28c473ebd61a6fda3bfebfcb2ec04413e15d
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-12-24 22:37:14 +0200

    Add Board render as ASCII

--- lib/game/board.ex
+++ lib/game/board.ex
@@ -14,6 +14,36 @@ defmodule TicTacToe.Game.Board do
   def new(name),
     do: %__MODULE__{
       name: name,
-      state: State.new(),
+      state: State.new()
     }
+
+  @doc """
+  Render game board as ASCII.
+  """
+  def render(%__MODULE__{state: %State{placements: placements}}) do
+    IO.puts(
+      "\n #{get_value(0, placements)} | #{get_value(1, placements)} | #{get_value(2, placements)} "
+    )
+
+    IO.puts("-----------")
+
+    IO.puts(
+      " #{get_value(3, placements)} | #{get_value(4, placements)} | #{get_value(5, placements)} "
+    )
+
+    IO.puts("-----------")
+
+    IO.puts(
+      " #{get_value(6, placements)} | #{get_value(7, placements)} | #{get_value(8, placements)} "
+    )
+  end
+
+  # Internal helpers
+  defp get_value(index, placements) when is_integer(index) do
+    case Enum.at(placements, index) do
+      :x -> "X"
+      :o -> "O"
+      _ -> "-"
+    end
+  end
 end
